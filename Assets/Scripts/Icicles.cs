c7dccccca3c7c0e70d83e2b7c790243188d0495a
Henrik Pedersen <henrikpedersen1@hotmail.com>
2015-11-10 21:24:37 +0100

    Started on the Icicles Feature
    :bear:

---
+++ Assets/Scripts/Icicles.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Icicles : MonoBehaviour {
+
+    int numberOfJumps = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        numberOfJumps++;
+        print(numberOfJumps);
+        col.transform.parent = transform;
+    }
+
+    void OnCollisionStay(Collision col)
+    {
+
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        print("Off we go");
+        col.transform.parent = null;
+    }
+}
