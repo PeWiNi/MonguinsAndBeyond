9d751f8e7c45b00ff6b22d821e8d4aea819d4fa9
Henrik Pedersen <henrikpedersen1@hotmail.com>
2015-11-10 16:17:51 +0100

    Added Ice Floe Feature Scene
    :octocat:

---
+++ Assets/Scripts/IceFloes.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IceFloes : MonoBehaviour
+{
+
+    Quaternion axisAlignRot;
+    // Use this for initialization
+    void Start()
+    {
+        axisAlignRot = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Whatever axis you want to align to
+        Vector3 worldAxis = new Vector3(0, 1, 0);
+        // rotate towards this axis
+        Vector3 worldAxisRelative = transform.TransformDirection(worldAxis);
+        axisAlignRot = Quaternion.FromToRotation(worldAxisRelative, worldAxis);
+        transform.rotation = Quaternion.Slerp(transform.rotation, axisAlignRot * transform.rotation, 1.0f * Time.deltaTime);
+    }
+}
