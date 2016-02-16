cd217065cbe44945f90044631fa5a6bd21965e91
Henrik Pedersen <henrikpedersen1@hotmail.com>
2016-02-16 19:55:47 +0100

    The environmental assets will now be aligned with the surface of the mesh :bangbang:

--- Assets/Scripts/EnvironmentPlacement.cs
+++ Assets/Scripts/EnvironmentPlacement.cs
@@ -18,6 +18,9 @@ public class EnvironmentPlacement : MonoBehaviour
         RandomizeEnvironment();
     }
 
+    /// <summary>
+    /// Based on the bounds of the mesh, create randomly placed environment assets.
+    /// </summary>
     void RandomizeEnvironment()
     {
         Vector3[] vertices = mesh.vertices;
@@ -25,24 +28,24 @@ public class EnvironmentPlacement : MonoBehaviour
         {
             //Get a random object from the environmentAssetsPool and assign it to the 'go' GameObject.
             GameObject go = environmentAssetsPool[(int)Random.Range(0f, environmentAssetsPool.Count)];
-            //Place the 'go' GameObject at a random location within the bounds of the segment, using Random.UnitSphere as starting position with direction towards the Island mesh.
-            Vector3 randomVertice = vertices[Random.Range(0, vertices.Length)];
+            //Access a random vertex from the vertices of the mesh.
+            Vector3 randomVertex = vertices[Random.Range(0, vertices.Length)];
+            //Get a point from the Random.onUnitSphere and apply a distance from the origin of that point (this is where the Ray later will have its origin).
             Vector3 direction = Random.onUnitSphere * Vector3.Distance(mesh.bounds.center, mesh.bounds.max * 2);
             //Ensures that we do not have a direction thats below the 'surface' area.
             while (direction.y <= 5f)
             {
                 direction = Random.onUnitSphere * Vector3.Distance(mesh.bounds.center, mesh.bounds.max * 2);
             }
-            //Randomize the rotation to diversify the environment.
-            Quaternion goRotation = Quaternion.EulerAngles(0f, Random.Range(-180f, 180f), 0f);
-            Debug.DrawLine(direction, randomVertice, Color.magenta, 5, false);
+            Debug.DrawLine(direction, randomVertex, Color.magenta, 15, false);
             RaycastHit hit;
-            if (Physics.Raycast(direction, randomVertice, out hit))
+            if (Physics.Raycast(direction, randomVertex, out hit))
             {
                 //Debug.DrawLine(hit.point, hit.point + hit.point.normalized, Color.blue, 100, false);
-                Debug.DrawLine(hit.point, direction, Color.blue, 100, false);
+                //Debug.DrawLine(hit.point, direction, Color.blue, 100, false);
+                //Debug.DrawLine(hit.point, hit.normal, Color.green, 100, false);
                 //Instantiate the 'go' GameObject
-                Instantiate(go, hit.point, goRotation);
+                Instantiate(go, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));//The FromToRotation(Vector3.up, hit.normal) ensures we align the 'go' GameObject along the surface of the mesh.
             }
         }
     }
