fd99500a92aac09a0e1c24233f3851451bf83fff
Henrik Pedersen <henrikpedersen1@hotmail.com>
2016-02-18 19:02:00 +0100

    Moved Environmental Scripts into a seperate folder
    uh.. and added another skybox material which I thought we ould use for
    the final fight in the end

--- Assets/Scripts/Environment/EnvironmentPlacement.cs
+++ Assets/Scripts/Environment/EnvironmentPlacement.cs
@@ -4,8 +4,14 @@ using System.Collections.Generic;
 
 public class EnvironmentPlacement : MonoBehaviour
 {
+    public enum Placement // your custom enumeration
+    {
+        Random,
+        Segmented,
+        None
+    };
+    public Placement placementState = Placement.None;//Default None.
 
-    public GameObject segmentMap;
     public List<GameObject> environmentAssetsPool;
     [Range(0f, 5000f)]
     public int maxNumberOfAssets;
@@ -15,7 +21,10 @@ public class EnvironmentPlacement : MonoBehaviour
     void Start()
     {
         mesh = GetComponent<MeshFilter>().mesh;
-        RandomizeEnvironment();
+        if (placementState == Placement.Random)
+            RandomizeEnvironment();
+        if (placementState == Placement.Segmented)
+            SegmentedPlacement(0);
     }
 
     /// <summary>
@@ -47,6 +56,37 @@ public class EnvironmentPlacement : MonoBehaviour
                 Instantiate(go, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));//The FromToRotation(Vector3.up, hit.normal) ensures we align the 'go' GameObject along the surface of the mesh.
                 amountWeNeed++;
             }
+        }
+    }
+
+    /// <summary>
+    /// Based on the following; 
+    /// 1) Divison (of the mesh, based on squares/circles). 
+    /// 2) Percentage(%) Fill Amount (the amount you want to fill this segment). 
+    /// 3) Location within segment (where do you want to place them, middle, upper corner etc).
+    /// The map will be filled with environmental assets at the geiven Location based on values.
+    /// </summary>
+    /// <param name="environmentAsset"></param>
+    void SegmentedPlacement(int environmentAsset)
+    {
+        Vector3[] vertices = mesh.vertices;
+        int amountWeNeed = 0;
+        //for (int i = 0; i < maxNumberOfAssets; i++)
+        while (amountWeNeed < vertices.Length)
+        {
+            //Get the environment asset you want to fill this segment with.
+            GameObject go = environmentAssetsPool[environmentAsset];
+            //Access a random vertex from the vertices of the mesh.
+            Vector3 randomVertex = vertices[amountWeNeed];
+            //Debug.DrawLine(direction, randomVertex, Color.magenta, 15, false);
+            RaycastHit hit;
+            if (Physics.Raycast(Vector3.up * 20f, randomVertex, out hit))
+            {
+                Debug.DrawLine(Vector3.up * 20f, randomVertex, Color.blue, 100, false);
+                //Instantiate the 'go' GameObject.
+                Instantiate(go, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));//The FromToRotation(Vector3.up, hit.normal) ensures we align the 'go' GameObject along the surface of the mesh.
+            }
+            amountWeNeed++;
         }
     }
 }
