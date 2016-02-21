d0d5998ea3b6e96db16cd830ba2fa4326fa7dc00
Henrik Pedersen <henrikpedersen1@hotmail.com>
2016-02-21 16:06:01 +0100

    Camouflage functionality implemented :sailboat:
    The basic functionality is there but it needs to be based on values of
    the Players n stuffs :)
    Also made some small changes to the Environment script
    And made a prefab out of the Island Model

---
+++ Assets/Scripts/Environment/Camouflage.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camouflage : MonoBehaviour
+{
+    public float duration = 0f;
+    public float durationInitialValue = 5f;
+    public float visibilityRangeRadius = 20f;// This should be affected based on the values of the Role selected (Supportor, Defender, Attacker).
+    public int durationPlantValue = 5;//Any additional plant adds 5sec to the 'duration'.
+    public int amountOfPlants = 2;//Should be in a Items Class or something.
+    public int plantsRequirement = 2;//The required number of plants before you can use camouflage.
+    public bool isCamouflaged = false;
+    public bool hasMovedFromCamouflagePoint = false;
+    public bool isPartlySpotted = false;
+
+    Vector3 camouflagePosition;
+    Vector3 playerCurrentPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("UpdateVisibilityState", 1f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check whether the Player wants to become camouflaged or not.
+        if (Input.GetKeyDown(KeyCode.C) && amountOfPlants >= plantsRequirement)
+        {
+            BeginCamouflage();
+        }
+        //Update Player Position.
+        if (playerCurrentPosition != gameObject.transform.position)
+        {
+            playerCurrentPosition = gameObject.transform.position;
+        }
+        //Should Check whether the Player has moved or used and Ability.
+        if (camouflagePosition != playerCurrentPosition && !hasMovedFromCamouflagePoint)
+        {
+            hasMovedFromCamouflagePoint = true;
+            StartCoroutine(CamouflageDuration());
+        }
+    }
+
+    /// <summary>
+    /// Camouflage the Player.
+    /// </summary>
+    void BeginCamouflage()
+    {
+        isCamouflaged = true;//The Player is now camouflaged.
+        duration = durationInitialValue + (durationInitialValue * amountOfPlants);//Set the duration.
+        amountOfPlants = 0;//Consume plants.
+        camouflagePosition = playerCurrentPosition;//Set the current player position to be the camouflage position.
+        Hide();
+    }
+
+    /// <summary>
+    /// Hide the Player.
+    /// </summary>
+    private void Hide()
+    {
+        Color colorOrigin = gameObject.GetComponent<Renderer>().material.color;
+        Color newColor = new Color(colorOrigin.r, colorOrigin.g, colorOrigin.b, 0f);
+        gameObject.GetComponent<Renderer>().material.color = newColor;
+    }
+
+    /// <summary>
+    /// Make the Player appear again.
+    /// </summary>
+    void Appear()
+    {
+        Color colorOrigin = gameObject.GetComponent<Renderer>().material.color;
+        Color newColor = new Color(colorOrigin.r, colorOrigin.g, colorOrigin.b, 1f);
+        gameObject.GetComponent<Renderer>().material.color = newColor;
+    }
+
+    /// <summary>
+    /// The Player will be 'almost' spotted resulting in being more vivisble.
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator PartlySpotted()
+    {
+        float partlySpottedValue = 0.8f;
+        float visibilityValue = 0.2f;
+        while (partlySpottedValue > visibilityValue)
+        {
+            Color colorOrigin = gameObject.GetComponent<Renderer>().material.color;
+            Color newColor = new Color(colorOrigin.r, colorOrigin.g, colorOrigin.b, partlySpottedValue);
+            gameObject.GetComponent<Renderer>().material.color = newColor;
+            yield return new WaitForSeconds(0.2f);
+            partlySpottedValue -= 0.1f;
+        }
+    }
+
+    /// <summary>
+    /// Start the camouflage duration.
+    /// When it ends the Player will appear again.
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator CamouflageDuration()
+    {
+        while (duration > 0f)
+        {
+            yield return new WaitForSeconds(1.0f);
+            duration--;
+        }
+        Appear();
+        isCamouflaged = false;
+        hasMovedFromCamouflagePoint = false;
+        yield return null;
+    }
+
+    void UpdateVisibilityState()
+    {
+        if (isCamouflaged)
+        {
+            Collider[] hitColliders = Physics.OverlapSphere(playerCurrentPosition, visibilityRangeRadius);
+            for (int i = 0; i < hitColliders.Length; i++)
+            {
+                if (!isPartlySpotted && hitColliders[i].transform.name == "SomeEnemy" && Vector3.Distance(playerCurrentPosition, hitColliders[i].transform.position) <= visibilityRangeRadius/2)
+                {
+                    print("Enemy is wthin spot range! GET AWAY OR YOU WILL BE SPOTTED!!!");
+                    isPartlySpotted = true;
+                    StartCoroutine(PartlySpotted());
+                }
+                else if (hitColliders[i].transform.name == "SomeEnemy" && Vector3.Distance(playerCurrentPosition, hitColliders[i].transform.position) > visibilityRangeRadius / 2)
+                {
+                    isPartlySpotted = false;
+                    Hide();
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(this.playerCurrentPosition, this.visibilityRangeRadius);
+    }
+}
--- Assets/Scripts/Environment/EnvironmentPlacement.cs
+++ Assets/Scripts/Environment/EnvironmentPlacement.cs
@@ -1,20 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public class EnvironmentPlacement : MonoBehaviour
 {
     public enum Placement // your custom enumeration
     {
         Random,
-        Segmented,
+        Area,
         None
     };
     public Placement placementState = Placement.None;//Default None.
-
     public List<GameObject> environmentAssetsPool;
     [Range(0f, 5000f)]
     public int maxNumberOfAssets;
+    public GameObject startingPoint;
+    public int radius;
+    public int assetID;
 
     private Mesh mesh;
 
@@ -23,8 +26,8 @@ public class EnvironmentPlacement : MonoBehaviour
         mesh = GetComponent<MeshFilter>().mesh;
         if (placementState == Placement.Random)
             RandomizeEnvironment();
-        if (placementState == Placement.Segmented)
-            SegmentedPlacement(0);
+        if (placementState == Placement.Area)
+            AreaPlacement(this.startingPoint, this.radius, this.maxNumberOfAssets, this.assetID);
     }
 
     /// <summary>
@@ -33,8 +36,16 @@ public class EnvironmentPlacement : MonoBehaviour
     void RandomizeEnvironment()
     {
         Vector3[] vertices = mesh.vertices;
+        //Convert vertices to correct scaling.
+        for (var i = 0; i < vertices.Length; i++)
+        {
+            Vector3 vertex = mesh.vertices[i];
+            vertex.x = vertex.x * transform.localScale.x;
+            vertex.y = vertex.y * transform.localScale.y;
+            vertex.z = vertex.z * transform.localScale.z;
+            vertices[i] = vertex;
+        }
         int amountWeNeed = 0;
-        //for (int i = 0; i < maxNumberOfAssets; i++)
         while (amountWeNeed < maxNumberOfAssets)
         {
             //Get a random object from the environmentAssetsPool and assign it to the 'go' GameObject.
@@ -42,17 +53,15 @@ public class EnvironmentPlacement : MonoBehaviour
             //Access a random vertex from the vertices of the mesh.
             Vector3 randomVertex = vertices[Random.Range(0, vertices.Length)];
             //Get a point from the Random.onUnitSphere and apply a distance from the origin of that point (this is where the Ray later will have its origin).
-            Vector3 direction = Random.onUnitSphere * Vector3.Distance(mesh.bounds.center, mesh.bounds.max * 2);
+            Vector3 origin = Random.onUnitSphere * Vector3.Distance(mesh.bounds.center, mesh.bounds.max * 2);
             //Ensures that we do not have a direction thats below the 'surface' area.
-            while (direction.y <= 5f)
+            while (origin.y <= 5f)
             {
-                direction = Random.onUnitSphere * Vector3.Distance(mesh.bounds.center, mesh.bounds.max * 2);
+                origin = Random.onUnitSphere * Vector3.Distance(mesh.bounds.center, mesh.bounds.max * 2);
             }
-            //Debug.DrawLine(direction, randomVertex, Color.magenta, 15, false);
             RaycastHit hit;
-            if (Physics.Raycast(direction, randomVertex, out hit))
+            if (Physics.Raycast(origin, randomVertex, out hit))
             {
-                //Instantiate the 'go' GameObject.
                 Instantiate(go, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));//The FromToRotation(Vector3.up, hit.normal) ensures we align the 'go' GameObject along the surface of the mesh.
                 amountWeNeed++;
             }
@@ -61,32 +70,71 @@ public class EnvironmentPlacement : MonoBehaviour
 
     /// <summary>
     /// Based on the following; 
-    /// 1) Divison (of the mesh, based on squares/circles). 
-    /// 2) Percentage(%) Fill Amount (the amount you want to fill this segment). 
-    /// 3) Location within segment (where do you want to place them, middle, upper corner etc).
+    /// 1) GameObject Starting Point (this will be the main focus and spawning point for the assets). 
+    /// 2) Radius (the boundary around the spawning point, using a sphere). 
+    /// 3) Amount Of Assets to spawn within this Radius boundary.
+    /// 4) The Asset ID to spawn at random locations within the Radius boundary.
     /// The map will be filled with environmental assets at the geiven Location based on values.
     /// </summary>
     /// <param name="environmentAsset"></param>
-    void SegmentedPlacement(int environmentAsset)
+    void AreaPlacement(GameObject startingPoint, float radius, int amountOfAssets, int assetID)
     {
-        Vector3[] vertices = mesh.vertices;
-        int amountWeNeed = 0;
-        //for (int i = 0; i < maxNumberOfAssets; i++)
-        while (amountWeNeed < vertices.Length)
+        //Get a collection of all colliers touched, because we only want to spawn assets on those.
+        Collider[] hitColliders = Physics.OverlapSphere(startingPoint.GetComponent<MeshFilter>().mesh.bounds.center, radius);
+        /* 
+         * We create a Dictionary<Mesh, Vector3[]> to receive the vertices that will be within the Radius to later acces them randomly.
+         * Mesh, is the mesh of the hitColliders GameObjects, Vector3[] will be the vertices that's within the radius on the mesh.
+         */
+        Dictionary<Mesh, Vector3[]> dictionary = new Dictionary<Mesh, Vector3[]>();
+        #region RecalcVerticesScale
+        for (int h = 0; h < hitColliders.Length; h++)
         {
-            //Get the environment asset you want to fill this segment with.
-            GameObject go = environmentAssetsPool[environmentAsset];
-            //Access a random vertex from the vertices of the mesh.
-            Vector3 randomVertex = vertices[amountWeNeed];
-            //Debug.DrawLine(direction, randomVertex, Color.magenta, 15, false);
-            RaycastHit hit;
-            if (Physics.Raycast(Vector3.up * 20f, randomVertex, out hit))
+            Mesh hitColliderMesh = hitColliders[h].gameObject.GetComponent<MeshFilter>().mesh;
+            List<Vector3> vertices = new List<Vector3>();
+            print("Before = " + vertices.Count);
+            //Convert vertices to correct scaling.
+            for (var i = 0; i < hitColliderMesh.vertexCount; i++)
             {
-                Debug.DrawLine(Vector3.up * 20f, randomVertex, Color.blue, 100, false);
-                //Instantiate the 'go' GameObject.
-                Instantiate(go, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));//The FromToRotation(Vector3.up, hit.normal) ensures we align the 'go' GameObject along the surface of the mesh.
+                Vector3 vertex = hitColliderMesh.vertices[i];
+                //We only want to add the vertices that are within the radius.
+                if (Vector3.Distance(hitColliderMesh.bounds.center, vertex) > radius)
+                    continue;
+                vertex.x = vertex.x * transform.localScale.x;
+                vertex.y = vertex.y * transform.localScale.y;
+                vertex.z = vertex.z * transform.localScale.z;
+                vertices.Add(vertex);
             }
-            amountWeNeed++;
+            dictionary.Add(hitColliderMesh, vertices.ToArray());
+            print("After = " + vertices.Count);
         }
+        #endregion
+        //Get the environment asset you want to fill this area with.
+        GameObject go = environmentAssetsPool[assetID];
+        int amountWeNeed = 0;
+        while (amountWeNeed < amountOfAssets)
+        {
+            //Select a random mesh GameObject between the ones that resides in the 'dictionary' variable, to spawn assets on.
+            List<Mesh> keyList = new List<Mesh>(dictionary.Keys);
+            Mesh mesh = keyList[Random.Range(0, keyList.Count-1)];
+            //Get the vertices of the 'mesh' GameObject.
+            Vector3[] vertices = mesh.vertices;
+            //Set the origin Vector3 to be the Center of the 'mesh' GameObject.
+            Vector3 origin = mesh.bounds.center;
+            //Get a random vertex.
+            Vector3 randomVertex = vertices[Random.Range(0, vertices.Length)];
+            RaycastHit hit;
+            if (Physics.Raycast(randomVertex, randomVertex, out hit))//***Should ignore all other layers but the ground***. 
+            {
+                Debug.DrawRay(randomVertex, origin, Color.magenta, 100f);
+                Instantiate(go, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));//The FromToRotation(Vector3.up, hit.normal) ensures we align the 'go' GameObject along the surface of the mesh.
+                amountWeNeed++;
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(this.startingPoint.GetComponent<MeshFilter>().sharedMesh.bounds.center, this.radius);
     }
 }
