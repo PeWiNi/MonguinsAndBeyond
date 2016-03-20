a410eddc8a323f1288973cdd07ae78158b5eabd0
Henrik Pedersen <henrikpedersen1@hotmail.com>
2016-03-20 21:36:15 +0100

    Working on Cwatapult twap

---
+++ Assets/Scripts/Environment/PlayerEffects/PlayerBehaviour.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PlayerBehaviour : NetworkBehaviour
+{
+    //Catapult variables.
+    public bool isThrown = false;
+    float thrust = 0f;
+    Vector3 origin = Vector3.zero;//Angle should between 30-60 Degrees based on player current position.
+    float radius = 0f;
+
+    //Slip variables.
+    public bool isSlipping = false;
+    //float thrust = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (isThrown)
+        {
+            print("WHY NOOO!");
+            Vector3 randomDirection = new Vector3(transform.position.x + Random.Range(30f, 60f), transform.position.y, transform.position.z + Random.Range(30f, 60f));
+            //transform.GetComponent<Rigidbody>().AddExplosionForce(thrust, origin, radius, thrust / 10, ForceMode.Impulse);//Explosion
+            transform.GetComponent<Rigidbody>().AddForce(transform.up * thrust + randomDirection, ForceMode.Impulse);//Force
+            isThrown = false;
+        }
+    }
+
+    /// <summary>
+    /// Player was thrown #Catapult.
+    /// </summary>
+    /// <param name="thrust"></param>
+    /// <param name="direction"></param>
+    /// <param name="triggerRadius"></param>
+    //public void PlayerThrown(float thrust, Vector3 origin, float triggerRadius)//Explosion
+    public void PlayerThrown(float thrust)//Force
+    {
+        if (!isLocalPlayer || isServer)
+            return;
+        print("Holy cow!");
+        //this.origin = origin;
+        this.thrust = thrust;
+        //this.radius = triggerRadius;
+        isThrown = true;
+        CmdStun(1);
+    }
+
+    /// <summary>
+    /// Player slipped
+    /// </summary>
+    /// <param name="effectDuration"></param>
+    /// <param name="thrust"></param>
+    public void PlayerSlipped(int effectDuration, float thrust)
+    {
+        if (!isLocalPlayer || isServer)
+            return;
+        if (!isSlipping)
+        {
+            this.thrust = thrust;
+            StartCoroutine(Slipping(effectDuration, thrust));
+        }
+    }
+
+    IEnumerator Slipping(int effectDuration, float thrust)
+    {
+        isSlipping = true;
+        transform.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * thrust, ForceMode.Impulse);
+        CmdStun(effectDuration);
+        while (effectDuration > 0)
+        {
+            yield return new WaitForSeconds(1f);
+            effectDuration--;
+        }
+        isSlipping = false;
+    }
+
+    [Command]
+    void CmdStun(int effectDuration)
+    {
+        transform.GetComponent<PlayerStats>().Stun(effectDuration);
+    }
+}
--- Assets/Scripts/Environment/Trap_VineTree.cs
+++ Assets/Scripts/Environment/Trap_VineTree.cs
@@ -6,27 +6,33 @@ public class Trap_VineTree : Trap
     [Tooltip("The force applied to the Player")]
     public float thrust = 50f;
     [Tooltip("The SphereCollider used for determine the triggerRadius area")]
-    public SphereCollider attachedCollider = null;
+    public SphereCollider crownTriggerCollider = null;
+    [Tooltip("The Direction which players will be thrown")]
+    public Vector3 direction;
 
     // Use this for initialization
     void Start()
     {
-        attachedCollider = GetComponent<SphereCollider>();
+        if (crownTriggerCollider == null)
+            crownTriggerCollider = GetComponentInChildren<SphereCollider>();
     }
 
     void OnTriggerEnter(Collider _collider)
     {
         if (_collider.transform.tag == "Player")
         {
+            print("HEJSA !");
             //We search for all Players in the triggerRadius to determine who should be thrown.
-            Collider[] playersWithinRange = Physics.OverlapSphere(transform.position, attachedCollider.radius);
+            Collider[] playersWithinRange = Physics.OverlapSphere(transform.position, 2f);
             foreach (Collider col in playersWithinRange)
             {
+                print("col.gameObject = " + col.gameObject);
                 if (col.transform.tag == "Player")
                 {
                     print("SHIT!");
                     //_collider.gameObject.GetComponent<Throw>().PlayerThrown(thrust, transform.position, attachedCollider.radius);//Explosion
-                    _collider.gameObject.GetComponent<Throw>().PlayerThrown(thrust);//Force
+                    //_collider.gameObject.GetComponent<Throw>().PlayerThrown(thrust);//Force
+                    _collider.gameObject.GetComponent<PlayerBehaviour>().PlayerThrown(thrust);//Force
                 }
             }
         }
@@ -34,10 +40,19 @@ public class Trap_VineTree : Trap
 
     void OnDrawGizmos()
     {
-        if (attachedCollider != null)
+        if (crownTriggerCollider != null)
         {
             Gizmos.color = Color.blue;
-            Gizmos.DrawWireSphere(transform.position, attachedCollider.radius);
+            //Gizmos.DrawWireSphere(crownTriggerCollider.bounds.center, crownTriggerCollider.radius + crownTriggerCollider.transform.localScale.x * 1.5f);
+            Gizmos.DrawWireSphere(crownTriggerCollider.bounds.center, 2f);
         }
+    }
+
+    public Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Vector3 angles)
+    {
+        Vector3 dir = point - pivot; // get point direction relative to pivot
+        dir = Quaternion.Euler(angles) * dir; // rotate it
+        point = dir + pivot; // calculate rotated point
+        return point; // return it
     }
 }
