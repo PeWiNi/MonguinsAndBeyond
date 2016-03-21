55eef4579f0e54a45490e0ddc595cd1d2ef0c043
Henrik Pedersen <henrikpedersen1@hotmail.com>
2016-03-21 15:23:38 +0100

    More work on Catapult twap :hammer:

--- Assets/Scripts/Environment/Trap.cs
+++ Assets/Scripts/Environment/Trap.cs
@@ -6,11 +6,20 @@ public class Trap : NetworkBehaviour
 {
     [Tooltip("The duration of the Trap before it expires")]
     [Range(0, 60)]
+    [SyncVar]
     public int duration = 30;
     [Tooltip("The duration of the effect of the Trap")]
+    [SyncVar]
     public int effectDuration = 30;
-    [Tooltip("The state of the Trap - Sprung or not")]
+    [Tooltip("Checks whether the Trap has been triggered")]
+    [SyncVar]
     public bool isTriggered = false;
+    [Tooltip("Determines whether or not this Trap is under construction by a Player")]
+    [SyncVar]
+    public bool isUnderConstruction = false;
+    [Tooltip("Determines whether the Trap is fully assembled")]
+    [SyncVar]
+    public bool isAssembled = false;
 
     // Use this for initialization
     void Start()
--- Assets/Scripts/Environment/Trap_VineTree.cs
+++ Assets/Scripts/Environment/Trap_VineTree.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 using System.Collections;
 
 public class Trap_VineTree : Trap
 {
+    public GameObject player;
     [Tooltip("The force applied to the Player")]
     public float thrust = 50f;
     [Tooltip("The SphereCollider used for determine the triggerRadius area")]
@@ -10,31 +12,75 @@ public class Trap_VineTree : Trap
     [Tooltip("The Direction which players will be thrown")]
     public Vector3 direction;
 
+    //Mouse Cursors + states
+    public Texture2D cursorFreeHand;
+    public Texture2D cursorGrab;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+    public bool isHoveringTrap = false;//Hovering over trap.
+    public bool isHoldingDownTrap = false;//Holding LMB Down.
+
     // Use this for initialization
     void Start()
     {
         if (crownTriggerCollider == null)
-            crownTriggerCollider = GetComponentInChildren<SphereCollider>();
+            crownTriggerCollider = GetComponent<SphereCollider>();
+    }
+
+    void Update()
+    {
+        TrapDirectionCursorState();
+    }
+
+    private void TrapDirectionCursorState()
+    {
+        if (Input.GetKey(KeyCode.Mouse0) && isHoveringTrap)
+        {
+            isHoldingDownTrap = true;
+            Cursor.SetCursor(cursorGrab, hotSpot, cursorMode);
+            Vector3 targetDir = player.transform.position - transform.position;
+            //float step = player.GetComponent<PlayerStats>().speed * Time.deltaTime;
+            float step = Vector3.Distance(player.transform.position, transform.position);
+            Vector3 newDir = Vector3.RotateTowards(transform.position, targetDir, step, 0.0F);//Original
+            transform.rotation = Quaternion.LookRotation(-newDir);
+        }
+        else
+        {
+            isHoldingDownTrap = false;
+            Cursor.SetCursor(null, Vector2.zero, cursorMode);
+        }
     }
 
     void OnTriggerEnter(Collider _collider)
     {
-        if (_collider.transform.tag == "Player")
+        if (_collider.transform.tag == "Player" && this.isAssembled && !this.isTriggered && !this.isUnderConstruction)
         {
-            print("HEJSA !");
+            this.isTriggered = true;
+            this.isAssembled = false;
             //We search for all Players in the triggerRadius to determine who should be thrown.
             Collider[] playersWithinRange = Physics.OverlapSphere(transform.position, 2f);
             foreach (Collider col in playersWithinRange)
             {
-                print("col.gameObject = " + col.gameObject);
                 if (col.transform.tag == "Player")
                 {
-                    print("SHIT!");
-                    //_collider.gameObject.GetComponent<Throw>().PlayerThrown(thrust, transform.position, attachedCollider.radius);//Explosion
-                    //_collider.gameObject.GetComponent<Throw>().PlayerThrown(thrust);//Force
-                    _collider.gameObject.GetComponent<PlayerBehaviour>().PlayerThrown(thrust);//Force
+                    _collider.gameObject.GetComponent<PlayerBehaviour>().PlayerThrown(thrust);
                 }
             }
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        Cursor.SetCursor(cursorFreeHand, hotSpot, cursorMode);
+        isHoveringTrap = true;
+    }
+
+    void OnMouseExit()
+    {
+        if (!isHoldingDownTrap)
+        {
+            Cursor.SetCursor(null, Vector2.zero, cursorMode);
+            isHoveringTrap = false;
         }
     }
 
@@ -43,16 +89,7 @@ public class Trap_VineTree : Trap
         if (crownTriggerCollider != null)
         {
             Gizmos.color = Color.blue;
-            //Gizmos.DrawWireSphere(crownTriggerCollider.bounds.center, crownTriggerCollider.radius + crownTriggerCollider.transform.localScale.x * 1.5f);
             Gizmos.DrawWireSphere(crownTriggerCollider.bounds.center, 2f);
         }
-    }
-
-    public Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Vector3 angles)
-    {
-        Vector3 dir = point - pivot; // get point direction relative to pivot
-        dir = Quaternion.Euler(angles) * dir; // rotate it
-        point = dir + pivot; // calculate rotated point
-        return point; // return it
     }
 }
