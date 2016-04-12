7e4a9de00685c489aebd9899a93e2a0f5fa4b4ac
Tornady <clbw@itu.dk>
2016-04-12 12:36:08 +0200

    :jeans: Better Upload this Scene

---
+++ Assets/Scripts/Abilities/HealForce.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 
+///  Heal force - ability targets only friendly characters. Heals 50-250 HP over 3 sec depending on skill and herbs used in the ability. Max range 20 units. 1 herb heals instantly for 50HP, 2->4 herb heal over time (50 at first and 50 more for each 'tic'). No CD; instant application; requires herbs to cast
+/// </summary>
+public class HealForce : Ability {
+}
