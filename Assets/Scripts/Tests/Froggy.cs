1f592b6f0044c2a06a4db208e5b5699c9684f8b3
Tornady <clbw@itu.dk>
2016-04-24 15:48:32 +0200

    GameScene makeover (and Froggy funzies) :frog:

---
+++ Assets/Scripts/Tests/Froggy.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(Animator))]
+public class Froggy : NetworkBehaviour {
+
+    Animator animator;
+
+    //Idle state variables.
+    public float timeToIdle = 5;
+    float currentTime = 0f;
+    bool isIdle = false;
+
+    // Use this for initialization
+    void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        #region Idle
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle") && !isIdle) {
+            isIdle = true;
+            currentTime = (float)Network.time + timeToIdle;
+            CheckIdle();
+        } else if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Idle") && isIdle) {
+            isIdle = false;
+        } if (isIdle) {
+            CheckIdle();
+        }
+        #endregion
+    }
+    
+    /*void OnAnimatorMove() {
+        Animator animator = GetComponent<Animator>();
+
+        if (animator) {
+            Vector3 newPosition = transform.position;
+            newPosition.z += animator.GetFloat("Runspeed") * Time.deltaTime;
+            transform.position = newPosition;
+        }
+    }*/
+
+    void CheckIdle() {
+        if ((float)Network.time > currentTime) {
+            int rand = Random.Range(0, 20) + 1;
+            if (rand <= 5) 
+                animator.SetTrigger("idleLookRight");
+            else if (rand <= 10) 
+                animator.SetTrigger("idleLookLeft");
+            else if (rand <= 15) 
+                animator.SetTrigger("idleLookAround");
+            else if (rand > 20)
+                animator.SetTrigger("idleJump");
+            currentTime = (float)Network.time + timeToIdle + Random.Range(0, 5);
+        }
+    }
+
+    void OnCollisionEnter(Collision _col) {
+        if (_col.gameObject.tag == "Player") {
+            Physics.IgnoreCollision(_col.collider, GetComponent<Collider>());
+        }
+    }
+}
---
+++ Assets/Scripts/Tests/FroggyTrack.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class FroggyTrack : NetworkBehaviour {
+    [SerializeField]
+    GameObject Track;
+    [SyncVar]
+    bool statusOn = false;
+    bool on = false;
+    [SerializeField]
+    Material frogOn;
+    [SerializeField]
+    Material frogOff;
+
+    [SerializeField]
+    SkinnedMeshRenderer frog;
+	
+	// Update is called once per frame
+	void Update () {
+	    if(on != statusOn) {
+            on = statusOn;
+            frog.material = statusOn ? frogOn : frogOff;
+            Track.SetActive(statusOn);
+        }
+	}
+
+    void OnTriggerEnter(Collider _col) {
+        if(isServer && _col.tag == "Player") {
+            statusOn = !statusOn;
+        }
+    }
+}
