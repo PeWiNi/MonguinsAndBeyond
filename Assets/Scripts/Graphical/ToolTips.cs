4bbdf038adf17b71fcb925050ef25f74e53662fd
Tornady <clbw@itu.dk>
2016-04-27 14:53:56 +0200

    :baby: New Trap icons
    BTW: to host Press F1, to make a server press F2

---
+++ Assets/Scripts/Graphical/ToolTips.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToolTips : MonoBehaviour {
+    [SerializeField]
+    string toolTipText = "";
+    [SerializeField]
+    Texture2D backgroundTexture;
+
+    string currentToolTipText = "";
+    GUIStyle guiStyleFore;
+    GUIStyle guiStyleBack;
+
+    int clickLeft = 0;
+    bool mouseIsOnIT = false;
+
+    void Start() {
+        guiStyleFore = new GUIStyle();
+        guiStyleFore.normal.textColor = Color.white;
+        guiStyleFore.alignment = TextAnchor.UpperCenter;
+        guiStyleFore.wordWrap = true;
+        guiStyleBack = new GUIStyle();
+        guiStyleBack.normal.background = backgroundTexture;
+        guiStyleBack.normal.textColor = Color.black;
+        guiStyleBack.alignment = TextAnchor.UpperCenter;
+        guiStyleBack.wordWrap = true;
+    }
+
+    void OnMouseEnter() {
+        mouseIsOnIT = true;
+        currentToolTipText = toolTipText;
+    }
+
+    void OnMouseExit() {
+        mouseIsOnIT = false;
+        currentToolTipText = "";
+    }
+
+    void OnGUI() {
+        if (currentToolTipText != "") {
+            var x = Event.current.mousePosition.x;
+            var y = Event.current.mousePosition.y;
+            GUI.Label(new Rect(x, y - 101, 100, 100), currentToolTipText, guiStyleBack);
+            GUI.Label(new Rect(x - 1, y - 100, 100, 100), currentToolTipText, guiStyleFore);
+        }
+    }
+}
